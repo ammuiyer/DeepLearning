d45c253faf4102600a0d8e3d47cc1a7ccf14a753
ammuiyer <ammu.iyer5@gmail.com>
2022-02-28 08:09:59 -0800

    updated w new dataset b/c other dataset was too large

--- tester.py
+++ tester.py
@@ -1,17 +1,35 @@
 print("test")
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-from sklearn.metrics import confusion_matrix
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
-import itertools
 import os
-import random
+import PIL
+import PIL.Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
-train_path = "train/melanoma"
-test_path = "chest_xray/test"
+import pathlib
+dataset_url = "/Users/meenakshiiyer/CodingWork/melanomaDetection/cifar-10-batches-py"
+cifar10 = tf.keras.utils.get_file(origin=dataset_url,
+                                   fname='flower_photos',
+                                   untar=True)
+cifar10 = pathlib.Path(cifar10)
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i])
+    # The CIFAR labels happen to be arrays, 
+    # which is why you need the extra index
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
