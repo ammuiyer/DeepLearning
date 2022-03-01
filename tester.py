2293b6c36eb3422a45996407b08e8775316dda1c
ammuiyer <ammu.iyer5@gmail.com>
2022-02-28 22:44:14 -0800

    my code is not working?

--- tester.py
+++ tester.py
@@ -1,35 +1,23 @@
 print("test")
-import numpy as np
-import os
-import PIL
-import PIL.Image
 import tensorflow as tf
-import tensorflow_datasets as tfds
+
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
-import pathlib
-dataset_url = "/Users/meenakshiiyer/CodingWork/melanomaDetection/cifar-10-batches-py"
-cifar10 = tf.keras.utils.get_file(origin=dataset_url,
-                                   fname='flower_photos',
-                                   untar=True)
-cifar10 = pathlib.Path(cifar10)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-# Normalize pixel values to be between 0 and 1
+#making the pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i])
-    # The CIFAR labels happen to be arrays, 
-    # which is why you need the extra index
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
+#making the model
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+print("test2")
+print(model.summary())
